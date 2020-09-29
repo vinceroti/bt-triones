10c554477751a11d90e90cbef1685c8c2e214f48
Vince Roti <vince.roti@gmail.com>
2020-09-28 19:51:38 -0700

    script to turn on triones

---
+++ index.js
@@ -0,0 +1,29 @@
+var bt = require('@abandonware/noble');
+
+bt.startScanning([], true);
+bt.on("stateChange", (e) => {
+  console.log(e);
+});
+
+bt.on("discover", async (peripheral) => {
+  if (peripheral.advertisement.localName && peripheral.advertisement.localName.includes("Triones")) {
+    console.log('found triones');
+    await peripheral.connect();
+    await bt.stopScanningAsync();
+    peripheral.once("connect", async () => {
+      peripheral.discoverAllServicesAndCharacteristics((error, services, characteristics) => {
+        // console.log(characteristics)
+        characteristics.forEach(c => {
+          if (c.properties.includes("write")) {
+            const b = Buffer.from([0xCC, 0x23, 0x33]);
+            console.log(c)
+            c.write(b, false, (e) => {
+              console.log('write')
+            })
+          }
+
+        })
+      });
+    });
+  }
+});
